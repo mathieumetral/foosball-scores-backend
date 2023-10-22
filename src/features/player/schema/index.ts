3db5a9b7a74b6b608e203b0843ae0bda68a89bf7
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-22 14:42:31 +0200

    feat(player): add players query

--- src/features/player/schema/index.ts
+++ src/features/player/schema/index.ts
@@ -1,1 +1,2 @@
 import '@features/player/schema/types';
+import '@features/player/schema/queries';
---
+++ src/features/player/schema/queries/players.ts
@@ -0,0 +1,11 @@
+import {schemaBuilder} from "@app/schema/builder";
+import {Player} from "@features/player/data/player";
+import {resolveWindowedConnection} from "@lib/pagination/utils";
+
+schemaBuilder.queryField('players', t => t.connection({
+    type: Player,
+    resolve: (root, args) => resolveWindowedConnection({args}, ({offset, limit}) => ({
+        items: Player.getMany(offset, limit),
+        totalCount: Player.count()
+    }))
+}))
