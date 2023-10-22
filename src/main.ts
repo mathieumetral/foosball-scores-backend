046ee16252912dda5dfcc52919e187140ea6ed7d
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-22 12:35:33 +0200

    chore(format): add prettier configuration (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { createServer } from "node:http";
+import {createServer} from 'node:http';
 
 const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World');
 });
 
