3b3edbb19d487351e47b99fb27774fb03233a9e2
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-22 13:29:07 +0200

    feat: add game smart data object + memory data source

---
+++ src/features/game/schema/types.ts
@@ -0,0 +1,35 @@
+import {schemaBuilder} from '@app/schema/builder';
+import {Team} from '@features/team/data/team';
+import {Game} from '@features/game/data/game';
+import {GameSide} from '@features/game/data/game-side';
+
+schemaBuilder.objectType(GameSide, {
+  name: 'GameSide',
+  fields: t => ({
+    team: t.field({
+      type: Team,
+      resolve: gameSide => gameSide.getTeam(),
+    }),
+    score: t.int({
+      resolve: gameSide => gameSide.getScore(),
+    }),
+  }),
+});
+
+schemaBuilder.node(Game, {
+  name: 'Game',
+  id: {
+    resolve: game => game.getId(),
+  },
+  loadMany: ids => ids.map(id => Game.get(id)),
+  fields: t => ({
+    leftSide: t.field({
+      type: GameSide,
+      resolve: game => game.getLeftSide(),
+    }),
+    rightSide: t.field({
+      type: GameSide,
+      resolve: game => game.getRightSide(),
+    }),
+  }),
+});
