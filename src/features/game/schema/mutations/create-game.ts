f401c9670c767c1ddb007d4821c4b6147b556b94
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-22 15:13:42 +0200

    feat(game): add create mutation (#15)
    * feat(player): add getOrCreateByName in smart data object
    
    * feat(team): add getOrCreateByPlayerName in smart data object
    
    * feat(game): add create in smart data object
    
    * feat(game): add create mutation

---
+++ src/features/game/schema/mutations/create-game.ts
@@ -0,0 +1,46 @@
+import {schemaBuilder} from '@app/schema/builder';
+import {Game} from '@features/game/data/game';
+
+export interface CreateGameSideInput {
+  playerName: string;
+  score?: number | null | undefined;
+}
+
+export interface CreateGameInput {
+  leftSide: CreateGameSideInput;
+  rightSide: CreateGameSideInput;
+}
+
+// Inputs
+const CreateGameSideInputRef = schemaBuilder.inputRef<CreateGameSideInput>('CreateGameSideInput').implement({
+  fields: t => ({
+    playerName: t.string({
+      required: true,
+    }),
+    score: t.int(),
+  }),
+});
+
+const CreateGameInputRef = schemaBuilder.inputRef<CreateGameInput>('CreateGameInput').implement({
+  fields: t => ({
+    leftSide: t.field({
+      type: CreateGameSideInputRef,
+      required: true,
+    }),
+    rightSide: t.field({
+      type: CreateGameSideInputRef,
+      required: true,
+    }),
+  }),
+});
+
+// Mutation
+schemaBuilder.mutationField('createGame', t =>
+  t.field({
+    type: Game,
+    args: {
+      input: t.arg({type: CreateGameInputRef, required: true}),
+    },
+    resolve: (parent, {input}) => Game.create(input),
+  })
+);
---
+++ src/features/game/schema/mutations/index.ts
@@ -0,0 +1,1 @@
+import '@features/game/schema/mutations/create-game';
