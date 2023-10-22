b9b50d0d4f23fcf67dbc96f533603d5257193e64
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-22 14:57:05 +0200

    fix: run format

--- src/features/player/schema/queries/players.ts
+++ src/features/player/schema/queries/players.ts
@@ -1,11 +1,14 @@
-import {schemaBuilder} from "@app/schema/builder";
-import {Player} from "@features/player/data/player";
-import {resolveWindowedConnection} from "@lib/pagination/utils";
+import {schemaBuilder} from '@app/schema/builder';
+import {Player} from '@features/player/data/player';
+import {resolveWindowedConnection} from '@lib/pagination/utils';
 
-schemaBuilder.queryField('players', t => t.connection({
+schemaBuilder.queryField('players', t =>
+  t.connection({
     type: Player,
-    resolve: (root, args) => resolveWindowedConnection({args}, ({offset, limit}) => ({
+    resolve: (root, args) =>
+      resolveWindowedConnection({args}, ({offset, limit}) => ({
         items: Player.getMany(offset, limit),
-        totalCount: Player.count()
-    }))
-}))
+        totalCount: Player.count(),
+      })),
+  })
+);
