70bfcbdcbc03af75432be1e9966e0031c85da695
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-22 12:46:21 +0200

    feat(graphql): initialize graphql-yoga with basic schema

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import {createServer} from 'node:http';
+import {app} from '@app/index';
 
-const server = createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World');
-});
+const server = createServer(app);
 
 server.listen(4000, () => {
   console.log(`Server is running on http://localhost:4000`);
