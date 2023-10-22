13c7ef069a00e9f91727b98930a8318fdda2654f
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-22 14:35:03 +0200

    feat(team): add games connection (#13)

--- src/features/team/schema/types.ts
+++ src/features/team/schema/types.ts
@@ -1,6 +1,8 @@
 import {schemaBuilder} from '@app/schema/builder';
 import {Team} from '@features/team/data/team';
 import {Player} from '@features/player/data/player';
+import {Game} from '@features/game/data/game';
+import {resolveWindowedConnection} from '@lib/pagination/utils';
 
 schemaBuilder.node(Team, {
   name: 'Team',
@@ -12,6 +14,16 @@ schemaBuilder.node(Team, {
     players: t.field({
       type: [Player],
       resolve: team => team.getPlayers(),
+    }),
+    games: t.connection({
+      type: Game,
+      resolve: (team, args) =>
+        resolveWindowedConnection({args}, ({offset, limit}) => {
+          return {
+            items: team.getGames(offset, limit),
+            totalCount: team.countGames(),
+          };
+        }),
     }),
   }),
 });
