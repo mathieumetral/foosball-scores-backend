c479755ef894f0a768f9b9d86c9a75bafe0f0666
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-23 18:17:47 +0200

    feat(player): add order by in get all query

--- src/features/player/data/player.ts
+++ src/features/player/data/player.ts
@@ -1,10 +1,16 @@
 import {Team} from '@features/team/data/team';
 import {PlayerStats} from '@features/player/data/player-stats';
 import {getDataSourcePostgreSQL} from '@data/sources/postgresql';
+import {Prisma} from '@prisma/client';
 
 export interface PlayerData {
   id: string;
   name: string;
+}
+
+export enum PlayerOrderBy {
+  WINS_MOST,
+  WINS_LEAST,
 }
 
 export class Player {
@@ -25,11 +31,38 @@ export class Player {
     return new Player(data);
   }
 
-  static async getMany(offset: number, limit: number): Promise<Player[]> {
-    const data = await getDataSourcePostgreSQL().player.findMany({
-      skip: offset,
-      take: limit,
-    });
+  static async getMany(offset: number, limit: number, orderBy?: PlayerOrderBy | null | undefined): Promise<Player[]> {
+    let orderByClause: Prisma.Sql;
+
+    switch (orderBy) {
+      case PlayerOrderBy.WINS_LEAST:
+        orderByClause = Prisma.sql`wins."numberOfWins" ASC`;
+        break;
+      case PlayerOrderBy.WINS_MOST:
+      default:
+        orderByClause = Prisma.sql`wins."numberOfWins" DESC`;
+        break;
+    }
+
+    // WARNING: Directly casting the database result like this can be risky.
+    const data: PlayerData[] = await getDataSourcePostgreSQL().$queryRaw`
+        SELECT p.*, wins."numberOfWins"
+        FROM "Player" p
+        LEFT JOIN (
+            SELECT tp."playerId",
+                SUM(CASE
+                    WHEN g."leftTeamId" = tp."teamId" AND g."leftTeamScore" > g."rightTeamScore" THEN 1
+                    WHEN g."rightTeamId" = tp."teamId" AND g."rightTeamScore" > g."leftTeamScore" THEN 1
+                    ELSE 0
+                END) AS "numberOfWins"
+            FROM "TeamPlayer" tp
+            JOIN "Game" g ON g."leftTeamId" = tp."teamId" OR g."rightTeamId" = tp."teamId"
+            GROUP BY tp."playerId"
+        ) wins ON p."id" = wins."playerId"
+        ORDER BY ${orderByClause}
+        OFFSET ${offset}
+        LIMIT ${limit};
+    `;
     return data.map(entry => new Player(entry));
   }
 
