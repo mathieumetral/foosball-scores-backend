09608202efb394f5d73b7dcb864442ddcfd24d6a
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-24 17:21:29 +0200

    feat(graphql): add dateTime scalar

--- src/app/schema/builder.ts
+++ src/app/schema/builder.ts
@@ -1,15 +1,24 @@
 import SchemaBuilder from '@pothos/core';
 import RelayPlugin from '@pothos/plugin-relay';
 import {PageCursors} from '@lib/pagination/types';
+import {DateTimeResolver} from 'graphql-scalars';
 
 export const schemaBuilder = new SchemaBuilder<{
   Connection: {
     pageCursors: PageCursors;
   };
+  Scalars: {
+    DateTime: {
+      Input: Date;
+      Output: Date;
+    };
+  };
 }>({
   plugins: [RelayPlugin],
   relayOptions: {},
 });
+
+schemaBuilder.addScalarType('DateTime', DateTimeResolver, {});
 
 schemaBuilder.queryType();
 schemaBuilder.mutationType();
